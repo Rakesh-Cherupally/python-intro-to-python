490bd5b82f67b98e453f2a2e8334245f85382366
rcherupa <rakesh.cherupally@oracle.com>
2023-06-26 09:15:02 +0530

    problems 1,2,3 completed commit

--- Problems/problem_2_happy_birthday.py
+++ Problems/problem_2_happy_birthday.py
@@ -1,3 +1,14 @@
 """
 Ask for the user's name and then sing happy birthday to them.
 """
+name = input('what is your name?').strip().title()
+
+print(f'Happy Birthday {name}')
+print(f'Happy Birthday {name}')
+print(f'Happy Birthday {name}')
+print(f'Happy Birthday {name}')
+
+
+
+
+
--- Problems/problem_3_temperature_converter.py
+++ Problems/problem_3_temperature_converter.py
@@ -3,9 +3,9 @@ Given a temperature in Fahrenheit, return the temperature in Celsius
 """
 # Ask for a temperature in Fahrenheit
 temp_f = input("Temp in F: ")
-
+50
 # Calculate it in Celsius
-temp_c = None
+temp_c = int(temp_f)-32 * 5/ 9;
 
 # Tell the user what it is
-print("Temp in C: ")
+print(f"Temp in C: {temp_c} ")
